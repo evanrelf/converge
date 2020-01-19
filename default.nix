537d6b016815de66e0f167336a9a8a1b9d0a8685
Evan Relf <evan@evanrelf.com>
2020-01-19 11:06:11 -0800

    Fix building Docker image with Nix
    I was trying to change the "system" attribute of the derivation, after
    Nixpkgs had already done everything under the assumption I was building
    for "x86_64-darwin." I needed to configure the Nixpkgs function with the
    "system" attribute so that it propagates to everything else.
    
    I also managed to clean things up so that my overrides don't clobber
    each other.

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
-let
-  pkgs = import ./nix/nixpkgs.nix { config = {}; };
+{ system ? builtins.currentSystem
+, compiler ? "ghc865"
+}:
 
+let
   onlyBuild = drv: pkgs.haskell.lib.overrideCabal drv (drv: {
     doBenchmark = false;
     doCheck = false;
@@ -8,44 +10,44 @@ let
     doHaddock = false;
   });
 
-  haskellPackages = pkgs.haskellPackages.override {
-    overrides = new: old: {
-      fused-effects = onlyBuild (new.callPackage ./nix/fused-effects.nix {});
-      relude = onlyBuild (new.callPackage ./nix/relude.nix {});
+  src = pkgs.nix-gitignore.gitignoreSource [ ".git/" ] ./.;
+
+  config = {
+    packageOverrides = pkgs: rec {
+      haskellPackages =
+        pkgs.haskell.packages.${compiler}.override {
+          overrides = new: old: rec {
+            converge = new.callCabal2nix "converge" src {};
+            fused-effects =
+              onlyBuild (new.callPackage ./nix/fused-effects.nix {});
+            relude = onlyBuild (new.callPackage ./nix/relude.nix {});
+          };
+        };
     };
   };
 
-  converge =
-    let
-      src = pkgs.nix-gitignore.gitignoreSource [ ".git/" ] ./.;
-    in
-      haskellPackages.callCabal2nix "converge" src {};
+  pkgs = import ./nix/nixpkgs.nix { inherit config; inherit system; };
 
   dockerImage =
-    # TODO:
-    # 1. Figure out the idiomatic way of specifying a build or host platform
-    # 2. Make sure both the Docker image and the contents are built on Linux
-    # 3. Figure out how to prevent subsequent overrides from clobbering
-    #    eachother (see https://github.com/NixOS/nixpkgs/issues/26561)
-    #
-    # docker load -i (nix-build --no-link -A dockerImage) && docker run -it --rm converge:latest
+    # docker load --input $(nix-build --no-link -A dockerImage)
+    # docker run --interactive --tty --rm converge:latest
     let
-      linuxPkgs = pkgs.forceSystem "x86_64-linux" "x86_64";
+      linuxPkgs =
+        import ./nix/nixpkgs.nix { inherit config; system = "x86_64-linux"; };
     in
       linuxPkgs.dockerTools.buildImage {
         name = "converge";
         tag = "latest";
         contents =
-          linuxPkgs.haskell.lib.justStaticExecutables converge;
+          linuxPkgs.haskell.lib.justStaticExecutables
+            linuxPkgs.haskellPackages.converge;
         config = {
-          Cmd = "/bin/converge";
-          ExposedPorts = {
-            "8080/tcp" = {};
-          };
+          Entrypoint = "/bin/converge";
+          ExposedPorts = { "8080" = {}; };
         };
       };
 
-  shell = converge.env.overrideAttrs (old: {
+  shell = pkgs.haskellPackages.converge.env.overrideAttrs (old: {
     buildInputs = with pkgs; old.buildInputs ++ [
       cabal-install
       ghcid
@@ -53,7 +55,7 @@ let
     ];
   });
 in
-  { inherit converge;
+  { converge = pkgs.haskellPackages.converge;
     inherit dockerImage;
     inherit shell;
   }
