8d16469452c07443e8a44774ac73ba9c83236a4e
Evan Relf <evan@evanrelf.com>
2020-01-18 16:37:01 -0800

    Add health check endpoint

--- src/Converge.hs
+++ src/Converge.hs
@@ -29,6 +29,10 @@ type WebhookApi =
     :> Servant.GitHubEvent '[ 'GitHub.WebhookPullRequestEvent ]
     :> Servant.GitHubSignedReqBody '[Servant.JSON] GitHub.PullRequestEvent
     :> Servant.Post '[Servant.JSON] ()
+  :<|>
+  "health"
+    :> Servant.Summary "Health check"
+    :> Servant.Get '[Servant.PlainText] Text
 
 onPing
   :: Servant.RepoWebhookEvent
@@ -46,9 +50,13 @@ onPullRequest GitHub.WebhookPullRequestEvent (_, event) =
   putTextLn ("PullRequstEvent: " <> show event)
 onPullRequest _ _ = pass
 
+onHealthCheck :: Servant.Handler Text
+onHealthCheck = pure "All good"
+
 server :: Servant.Server WebhookApi
 server = onPing
     :<|> onPullRequest
+    :<|> onHealthCheck
 
 -- Stupid hack to make servant-github-webhook work
 -- https://github.com/tsani/servant-github-webhook/issues/13#issuecomment-408463124
