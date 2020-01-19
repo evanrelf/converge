7278aee4f30086795feebee4751ecaad8d578bdc
Evan Relf <evan@evanrelf.com>
2020-01-18 22:14:49 -0800

    WIP Build Docker image with Nix

--- default.nix
+++ default.nix
@@ -13,9 +13,35 @@ let
   };
 
   pkgs = import ./nix/nixpkgs.nix { inherit config; };
+
+  dockerImage =
+    # TODO:
+    # 1. Figure out the idiomatic way of specifying a build or host platform
+    # 2. Make sure both the Docker image and the contents are built on Linux
+    # 3. Figure out how to prevent subsequent overrides from clobbering
+    #    eachother (see https://github.com/NixOS/nixpkgs/issues/26561)
+    #
+    # docker load -i (nix-build --no-link -A dockerImage) && docker run -it --rm converge:latest
+    let
+      linuxPkgs = pkgs.forceSystem "x86_64-linux" "x86_64";
+    in
+      linuxPkgs.dockerTools.buildImage {
+        name = "converge";
+        tag = "latest";
+        contents =
+          linuxPkgs.haskell.lib.justStaticExecutables (linuxPkgs.haskellPackages.callCabal2nix "converge" src {});
+        config = {
+          Cmd = "/bin/converge";
+          ExposedPorts = {
+            "8080/tcp" = {};
+          };
+        };
+      };
 in
   rec {
     converge = pkgs.haskellPackages.converge;
+
+    inherit dockerImage;
 
     shell = converge.env.overrideAttrs (old: {
       buildInputs = with pkgs; old.buildInputs ++ [
