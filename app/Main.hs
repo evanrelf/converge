91442ffb7bc2a9ca312beb229b8a03a054544514
Evan Relf <evan@evanrelf.com>
2020-01-19 18:23:20 -0800

    Accept port as command-line argument

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,15 @@ module Main (main) where
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Servant
 import Servant (Context((:.)))
+import System.Environment (getArgs)
 
 import qualified Converge
 
 main :: IO ()
 main = do
+  args <- getArgs
   let host = "localhost"
-  let port = 8080
+  let port = fromMaybe 8080 (viaNonEmpty head args >>= readMaybe)
   let secret = "super-secret-code"
   putTextLn ("Running at http://" <> host <> ":" <> show port)
   Warp.run port
