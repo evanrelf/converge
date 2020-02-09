24e18df37fb616271091c8db170d64e93ee46b64
Evan Relf <evan@evanrelf.com>
2020-02-08 22:48:24 -0800

    Add Log effect

---
+++ src/Control/Effect/Log.hs
@@ -0,0 +1,32 @@
+module Control.Effect.Log
+  ( Severity (..)
+  , Log (..)
+  , log
+  -- Re-exports
+  , Algebra
+  , Effect
+  , Has
+  , run
+  )
+where
+
+
+import Control.Algebra (Algebra, Effect, HFunctor, Has, run, send)
+import GHC.Generics (Generic1)
+
+
+data Severity
+  = Info
+  | Warn
+  | Error
+  deriving stock (Eq, Ord, Show)
+
+
+data Log m k
+  = Log Severity Text (m k)
+  deriving stock (Functor, Generic1)
+  deriving anyclass (HFunctor, Effect)
+
+
+log :: Has Log sig m => Severity -> Text -> m ()
+log severity message = send (Log severity message pass)
