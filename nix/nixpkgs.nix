fbdef9dfc22230c876d2816183df0bd126d6a168
Evan Relf <evan@evanrelf.com>
2020-02-17 20:54:55 -0800

    Update pinned Nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,6 +1,6 @@
 let
-  # nixos-19.09 on 2020-01-11
-  rev = "9f453eb97ffe261ff93136757cd08b522fac83b7";
+  # nixos-19.09 on 2020-02-17
+  rev = "8731aaaf8b30888bc24994096db830993090d7c4";
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     sha256 = "16wdsazc7g09ibcxlqsa3kblzhbbpdpb6s29llliybw73cp37b9s";
