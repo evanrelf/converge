b97f9027e2f68d54271fda60a214ccd96a3b82cb
Evan Relf <evan@evanrelf.com>
2020-03-29 21:12:14 -0700

    Allow specifying desired log verbosity
    Also added "Vomit" verbosity level for raw JSON spew

--- src/Control/Effect/Log.hs
+++ src/Control/Effect/Log.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Control.Effect.Log
-  ( Severity (..)
+  ( Verbosity (..)
+  , printVerbosity
   , Log (..)
   , log
   -- Re-exports
@@ -15,19 +18,29 @@ import Control.Algebra (Algebra, Effect, HFunctor, Has, run, send)
 import GHC.Generics (Generic1)
 
 
-data Severity
-  = Debug
+data Verbosity
+  = Vomit
+  | Debug
   | Info
   | Warn
   | Error
   deriving stock (Eq, Ord, Show)
 
 
+printVerbosity :: IsString s => Verbosity -> s
+printVerbosity = \case
+  Vomit -> "[ VOMIT ]"
+  Debug -> "[ DEBUG ]"
+  Info  -> "[ INFO  ]"
+  Warn  -> "[ WARN  ]"
+  Error -> "[ ERROR ]"
+
+
 data Log m k
-  = Log Severity Text (m k)
+  = Log Verbosity Text (m k)
   deriving stock (Functor, Generic1)
   deriving anyclass (HFunctor, Effect)
 
 
-log :: Has Log sig m => Severity -> Text -> m ()
+log :: Has Log sig m => Verbosity -> Text -> m ()
 log severity message = send (Log severity message pass)
