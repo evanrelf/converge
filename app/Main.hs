0005882cc38163ea689d40a520895b72b569e04b
Evan Relf <evan@evanrelf.com>
2020-04-13 21:02:40 -0700

    Move `main` function to library

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,8 @@
-{-# LANGUAGE TypeApplications #-}
-
-
 module Main (main) where
 
-
-import qualified Network.Wai.Handler.Warp as Warp
-import qualified Servant
-import Servant (Context ((:.)))
-import System.Environment (getArgs)
 
 import qualified Converge
 
 
 main :: IO ()
-main = do
-  args <- getArgs
-  let host = "localhost"
-  let port = fromMaybe 8080 (readMaybe =<< viaNonEmpty head args)
-  let secret = "super-secret-code"
-
-  putTextLn ("Running at http://" <> host <> ":" <> show port)
-  Warp.run port
-    (Servant.serveWithContext
-      (Proxy @Converge.WebhookApi)
-      (Converge.gitHubKey (pure secret) :. Servant.EmptyContext)
-      Converge.server)
+main = Converge.main
