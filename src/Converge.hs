c7325b8fa44e43a7d026199355d7b7c9e46c0434
Evan Relf <evan@evanrelf.com>
2020-04-24 23:19:37 -0700

    Move `api` function to `Converge.Api` (now `run`)

--- src/Converge.hs
+++ src/Converge.hs
@@ -1,32 +1,7 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TypeApplications #-}
-
 module Converge where
 
-import Converge.Api (Api, gitHubKey, server)
-import Data.String.Interpolate (i)
-import qualified Network.Wai.Handler.Warp as Warp
-import Servant (Context ((:.)))
-import qualified Servant
-import Prelude hiding (id)
+import qualified Converge.Api as Api
 
 
 main :: IO ()
-main = api
-
-
-api :: MonadIO m => m ()
-api = do
-  let host = "localhost" :: Text
-  let port = 7777
-  let secret = "super-secret-code"
-
-  putTextLn [i|Running at http://#{host}:#{port}|]
-  liftIO $ Warp.run port
-    (Servant.serveWithContext
-      (Proxy @Api)
-      (gitHubKey (pure secret) :. Servant.EmptyContext)
-      server)
+main = Api.run 7777 "super-secret-code"
