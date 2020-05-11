83c57b95c1d17dc385abc1147d05ab11d69dea04
Evan Relf <evan@evanrelf.com>
2020-05-10 22:27:02 -0700

    Finish API endpoint to dump state

--- src/Converge.hs
+++ src/Converge.hs
@@ -3,8 +3,15 @@ module Converge where
 import qualified Converge.Api as Api
 import Effect.Log (Verbosity (..), logToIO)
 import Polysemy
+import Polysemy.AtomicState (atomicStateToState)
+import Polysemy.State (evalState)
 
 
 main :: IO ()
 main = do
-  Api.run 7777 "super-secret-code" (runM . logToIO Vomit)
+  Api.run 7777 "super-secret-code"
+    ( runM
+    . evalState mempty
+    . atomicStateToState
+    . logToIO Vomit
+    )
