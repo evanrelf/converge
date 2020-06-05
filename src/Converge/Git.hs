c5be1f46809ed61b0be0848f6c0c5876b4dab7e2
Evan Relf <evan@evanrelf.com>
2020-06-04 23:38:46 -0700

    Don't use optics to apply `toText`
    It's easier to understand what's going on this way, especially since the
    strings now have names.

--- src/Converge/Git.hs
+++ src/Converge/Git.hs
@@ -4,16 +4,15 @@ module Converge.Git
   )
 where
 
-import Optics (_2, _3, over)
 import System.Exit (ExitCode)
 import qualified System.Process as Process
 
 
 git :: MonadIO m => [Text] -> m (ExitCode, Text, Text)
 git args = liftIO do
-  Process.readProcessWithExitCode "git" (toString <$> args) mempty
-    <&> over _2 toText
-    <&> over _3 toText
+  (exitCode, stdoutString, stderrString) <-
+    Process.readProcessWithExitCode "git" (toString <$> args) mempty
+  pure (exitCode, toText stdoutString, toText stderrString)
 
 
 git_ :: MonadIO m => [Text] -> m ()
