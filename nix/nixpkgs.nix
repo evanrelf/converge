8aa60bf6e96d5c0d79cd3c4f5605492019a4a05c
Evan Relf <evan@evanrelf.com>
2020-06-21 17:55:26 -0700

    WIP Update to GHC 8.8
    The main blocker right now is getting `github-webhooks` working.
    
    Tests fail for both `github-webhooks` version 0.13.0 and 0.14.0 due to
    `Prelude.read: no parse` exceptions.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,12 +1,10 @@
 args:
 
 let
-  # nixos-20.03 on 2020-03-23
-  rev = "b2935fbeceaea0b64df4401545d7c8ea29102120";
+  rev = "c27e54de99df793756a5314f8fd5dd3e49d31927";
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "1z1631v7q2c8mavy7xnvfx0wz34zd49jqmjg66nk0qgsi605m3qp";
+    sha256 = "0289m555hpmml5g6idicg1bckphww22p87s8qca2k046zkz0ykx0";
   };
-
 in
   import nixpkgs ({ config = {}; } // args)
